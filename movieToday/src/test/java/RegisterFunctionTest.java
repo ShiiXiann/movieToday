056b70fdd1b735b35997e3f9713cc12d1cb6ba8e
maghere33 <84113954+maghere33@users.noreply.github.com>
2022-02-23 09:36:46 +0800

    Changed POM file, Re-created JUnit files, change UserTest to default package

--- movieToday/src/test/java/RegisterFunctionTest.java
+++ movieToday/src/test/java/RegisterFunctionTest.java
@@ -5,30 +5,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RegisterFunctionTest {
-	private RegisterServlet registerServlet = new RegisterServlet();
-	private String username;
-	private String password;
-	private String email;
-	private String phoneNumber;
+    private RegisterServlet registerServlet = new RegisterServlet();
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
 
-	@BeforeEach
-	void setUp() throws Exception {
-		username = "idk";
-		password = "idk";
-		email = "idk@gmail.com";
-		phoneNumber = "999";
-	}
+    @BeforeEach
+    void setUp() throws Exception {
+        username = "idk";
+        password = "idk";
+        email = "idk@gmail.com";
+        phoneNumber = "999";
+    }
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+    @AfterEach
+    void tearDown() throws Exception {
+    }
 
-	@Test
-	void testRegisterFunction() {
-		//fail("Not yet implemented");
-		int i = registerServlet.RegisterFunction(username, password, email,phoneNumber);
-		//assertEquals(i,1);
-		assertTrue(i > 0);
-	}
+    @Test
+    void testRegisterFunction() {
+        //fail("Not yet implemented");
+        int i = registerServlet.RegisterFunction(username, password, email,phoneNumber);
+        //assertEquals(i,1);
+        assertTrue(i > 0);
+    }
 
 }
